5d8e5dcc3c9d5a1d5ace330785edf66cf0c5dea7
Dreamtowards <30804116+Dreamtowards@users.noreply.github.com>
2023-07-30 18:02:46 +0800

    main menu

--- Assets/Scripts/Eldaria.cs
+++ Assets/Scripts/Eldaria.cs
@@ -9,6 +9,6 @@ public class Eldaria
     //[MenuItem("Eldaria/MyTool/Do It in C#")]
     static void DoIt()
     {
-        EditorUtility.DisplayDialog("MyTool2", "Do It in C# !", "OK", "");
+        //EditorUtility.DisplayDialog("MyTool2", "Do It in C# !", "OK", "");
     }
 }
